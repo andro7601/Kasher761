ad1a735397dc2b33114e50e70875c44d31d45f40
Andria Shvelidze <shvelidze2007@gmail.com>
2026-06-28 00:13:54 +0400

    read and write per player networkIO seperate thread

---
+++ game-engine/src/main/java/com/game/network/ClientShard.java
@@ -0,0 +1,4 @@
+package com.game.network;
+
+public class ClientShard {
+}
---
+++ game-engine/src/main/java/com/game/network/PacketHandler.java
@@ -0,0 +1,16 @@
+package com.game;
+
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+
+public class PacketHandler {
+
+    void handle(ByteBuffer bytes, SocketAddress addrs) {
+
+    }
+
+    void Load(){
+
+    }
+}
